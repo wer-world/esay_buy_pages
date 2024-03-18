aa3d6a5117c7902ed8907bb14d7fe0ae30c46c51
idiotzzz <2537876081@qq.com>
2024-03-18 21:30:17 +0800

    冯天威第一次前端提交（首页左侧分类展示）

---
+++ util/leftNav.js
@@ -0,0 +1,24 @@
+new Vue({
+    el:'#leftNav',
+    data:{
+        categoryList1:[],
+        hoverIndex:-1
+    },
+    mounted(){
+        this.initCategoryList();
+    },
+    methods:{
+        initCategoryList(){
+            axios({
+                method:'post',
+                url:'/nginx/category/getCategoryList',
+                data:{
+                    'parentId':0
+                }
+            }).then((res)=>{
+                this.categoryList1 = res.data
+                console.log(this.categoryList1)
+            })
+        }
+    }
+})
