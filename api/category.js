3fff0e906350daeaf04de0915da49885f0bd1161
idiotzzz <2537876081@qq.com>
2024-03-23 00:04:27 +0800

    冯天威第七次提交(前端更新了分类管理)

--- api/category.js
+++ api/category.js
@@ -5,3 +5,36 @@ export function getCategoryList(value) {
         parentId: value
     })
 }
+
+export function getParentCategory(parentId){
+    return request.post('/category/getParentCategory',{
+        parentId:parentId
+    })
+}
+
+export function getCategory(id){
+    return request.post('/category/getCategory',{
+        id:id
+    })
+}
+
+export function addCategory(name,parentId,type){
+    return request.post('/category/addCategory',{
+        name:name,
+        parentId:parentId,
+        type:type
+    })
+}
+
+export function updateCategory(id,name){
+    return request.post('/category/updateCategory',{
+        id:id,
+        name:name
+    })
+}
+
+export function deleteCategory(id){
+    return request.post('/category/deleteCategory',{
+        id:id,
+    })
+}
